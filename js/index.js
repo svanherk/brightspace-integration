1a18e40eafcadb14da7a90e2a50db8ae33227e60
Martin Bayly <Martin.Bayly@D2L.com>
2018-06-13 16:38:28 -0700

    polyfill findIndex for IE11

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 import './polyfill/Array.prototype.includes.js';
+import './polyfill/Array.prototype.findIndex.js';
 import Lie from 'lie';
 if (typeof Promise === 'undefined') {
 	window.Promise = Lie;
