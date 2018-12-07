ca56630cb94f5bd3018523d0dae9c7ec292edee0
Dave Lockhart <dave.lockhart@d2l.com>
2018-12-07 14:21:56 -0500

    adding navigation

--- web-components/bsi.js
+++ web-components/bsi.js
@@ -9,11 +9,11 @@ window.D2L.FastDom = window.fastdom;
 
 import '../js/timing-debug.js';
 
-import { d2lfetch } from '../node_modules/d2l-fetch/src/index.js';
-window.d2lfetch = d2lfetch;
-
 import d2lIntl from 'd2l-intl';
 window.d2lIntl = d2lIntl;
+
+import { d2lfetch } from '../node_modules/d2l-fetch/src/index.js';
+window.d2lfetch = d2lfetch;
 
 // from bsi.html
 /*
@@ -58,18 +58,18 @@ import 'd2l-polymer-behaviors/d2l-gestures-swipe.js';
 import 'd2l-save-status/d2l-save-status.js';
 import 'd2l-simple-overlay/d2l-simple-overlay.js';
 import '@polymer/iron-icon/iron-icon.js';
+import 'd2l-navigation/d2l-navigation.js';
+import 'd2l-navigation/d2l-navigation-band.js';
+import 'd2l-navigation/d2l-navigation-button.js';
+import 'd2l-navigation/d2l-navigation-button-close.js';
+import 'd2l-navigation/d2l-navigation-button-notification-icon.js';
+import 'd2l-navigation/d2l-navigation-main-header.js';
+import 'd2l-navigation/d2l-navigation-main-footer.js';
+import 'd2l-navigation/d2l-navigation-link.js';
+import 'd2l-navigation/d2l-navigation-link-back.js';
+import 'd2l-navigation/d2l-navigation-link-image.js';
+import 'd2l-navigation/d2l-navigation-separator.js';
 /*
-<link rel="import" href="../bower_components/d2l-navigation/d2l-navigation.html">
-<link rel="import" href="../bower_components/d2l-navigation/d2l-navigation-band.html">
-<link rel="import" href="../bower_components/d2l-navigation/d2l-navigation-button.html">
-<link rel="import" href="../bower_components/d2l-navigation/d2l-navigation-button-close.html">
-<link rel="import" href="../bower_components/d2l-navigation/d2l-navigation-button-notification-icon.html">
-<link rel="import" href="../bower_components/d2l-navigation/d2l-navigation-main-header.html">
-<link rel="import" href="../bower_components/d2l-navigation/d2l-navigation-main-footer.html">
-<link rel="import" href="../bower_components/d2l-navigation/d2l-navigation-link.html">
-<link rel="import" href="../bower_components/d2l-navigation/d2l-navigation-link-back.html">
-<link rel="import" href="../bower_components/d2l-navigation/d2l-navigation-link-image.html">
-<link rel="import" href="../bower_components/d2l-navigation/d2l-navigation-separator.html">
 <link rel="import" href="../bower_components/d2l-users/all-imports.html">
-<link rel="import" href="navigation-icons.html">
 */
+import './navigation-icons.js';
