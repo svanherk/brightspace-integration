9e28f66020b661b6c2d503e13a6189a621929f47
Dave Lockhart <dave.lockhart@d2l.com>
2018-12-11 14:24:32 -0500

    putting fastdom and timing-debug back where they were

--- js/index.js
+++ js/index.js
@@ -18,5 +18,12 @@ window.D2L.FastDom = FastDom;
 
 import './timing-debug.js';
 
+window.D2L = window.D2L || {};
+
+import FastDom from './d2l-fastdom.js';
+window.D2L.FastDom = FastDom
+
+import './timing-debug.js';
+
 import '../bower_components/jquery-vui-change-tracking/changeTracker.js';
 import '../bower_components/jquery-vui-change-tracking/changeTracking.js';
