dfb4d58b626e7ac4640b3c436752a27f54e68810
Dave Lockhart <dave.lockhart@d2l.com>
2018-12-14 15:54:12 -0500

    fixing rebase artifact

--- js/index.js
+++ js/index.js
@@ -11,13 +11,6 @@ window.D2L.FastDom = FastDom;
 
 import './timing-debug.js';
 
-window.D2L = window.D2L || {};
-
-import FastDom from './d2l-fastdom.js';
-window.D2L.FastDom = FastDom;
-
-import './timing-debug.js';
-
 import '../bower_components/jquery-vui-change-tracking/changeTracker.js';
 import '../bower_components/jquery-vui-change-tracking/changeTracking.js';
 import '../bower_components/jquery-vui-collapsible-section/collapsibleSection.js';
