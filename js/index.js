ba957f514741f611c48b9d61c1beb90a435f4dc6
Dave Batiste <dave.batiste@d2l.com>
2018-12-19 15:09:59 -0500

    Remove JQuery collapsible-section.

--- js/index.js
+++ js/index.js
@@ -13,4 +13,3 @@ import './timing-debug.js';
 
 import '../bower_components/jquery-vui-change-tracking/changeTracker.js';
 import '../bower_components/jquery-vui-change-tracking/changeTracking.js';
-import '../bower_components/jquery-vui-collapsible-section/collapsibleSection.js';
