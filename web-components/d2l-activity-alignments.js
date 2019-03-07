8d8579f226f82d9918be523d5e8d3182349431de
Chris Kraljevic <chris.kraljevic@d2l.com>
2019-03-07 16:21:47 -0600

    Pulling in d2l-activity-alignment-tags from d2l-activity-alignments (#1260)

--- web-components/d2l-activity-alignments.js
+++ web-components/d2l-activity-alignments.js
@@ -1,2 +1,3 @@
 import 'd2l-activity-alignments/d2l-activity-alignments.js';
 import 'd2l-activity-alignments/d2l-select-outcomes.js';
+import 'd2l-activity-alignments/d2l-activity-alignment-tags.js';
