433425011a4123506cbbb72430be7db9ae043b13
Dave Lockhart <dave.lockhart@d2l.com>
2019-03-22 15:41:07 -0400

    Automatically tagging releases

---
+++ cli/autotag.js
@@ -0,0 +1,168 @@
+/* eslint no-console: "off" */
+
+'use strict';
+
+const chalk = require('chalk'),
+	Octokit = require('@octokit/rest'),
+	rally = require('rally');
+
+//  https://github.com/octokit/rest.js
+//  https://octokit.github.io/rest.js/
+const gh = new Octokit({
+	auth: process.env.GITHUB_TOKEN
+});
+
+const owner = 'dlockhart';
+const repo = 'brightspace-integration';
+const versionChecker = /^20\.[0-9]{2}\.(1|2|3|4|5|6|7|8|9|10|11|12)$/;
+const rallyVersionChecker = /^(20\.[0-9]{2}\.)([0-9]{2})$/;
+
+async function tryGetActiveDevelopmentRelease() {
+
+	console.log('  Fetching active development release from Rally...');
+
+	//  https://github.com/RallyTools/rally-node/wiki/User-Guide
+	const rallyApi = rally({
+		apiKey: process.env.RALLY_API_KEY,
+		apiVersion: 'v2.0',
+		requestOptions: {
+			headers: {
+				'X-RallyIntegrationName': 'BSI autotag',
+				'X-RallyIntegrationVendor': 'D2L Corporation',
+				'X-RallyIntegrationVersion': '1.0'
+			}
+		}
+	});
+
+	// Add 12 hours to the current time to account for branching occurring at
+	// noon on the last day of the release. After branching occurs, we consider
+	// it to be the following release.
+	const now = new Date();
+	now.setHours(now.getHours() + 12);
+
+	// format: 2019-03-16T03:59:59.000Z
+	const nowISO = now.toISOString();
+
+	// query to find release in active development
+	const releases = await rallyApi.query({
+		type: 'release',
+		limit: 1,
+		fetch: ['Name'],
+		query: rally.util.query.where('ReleaseStartDate', '<=', nowISO).and('ReleaseDate', '>', nowISO).and('Project.Name', '=', 'D2L')
+	});
+	if (releases.TotalResultCount !== 1) {
+		console.error(chalk.red('    Error: Unable to query for active development release in Rally.'));
+		return null;
+	}
+
+	let activeReleaseName = releases.Results[0].Name;
+	const match = rallyVersionChecker.exec(activeReleaseName);
+	if (!match) {
+		console.error(chalk.red(`    Error: Invalid Rally release name "${activeReleaseName}".`));
+		return null;
+	}
+
+	// strip off leading "0" from month part of the release
+	let monthPart = match[2];
+	if (monthPart.startsWith('0')) {
+		monthPart = monthPart.substring(1);
+	}
+	activeReleaseName = match[1] + monthPart;
+
+	console.log(chalk.green(`    Success! Active development release is: "${activeReleaseName}"`));
+
+	return activeReleaseName;
+
+}
+
+async function findMaxVersion(release) {
+
+	const releaseRe = new RegExp('^v' + release.replace('.', '\\.') + '-([0-9]+)$');
+
+	const options = gh.repos.listReleases.endpoint.merge({
+		'owner': owner,
+		'repo': repo,
+		'per_page': 100
+	});
+	const releases = await gh.paginate(
+		options,
+		response => response.data.map(release => release.tag_name)
+	);
+
+	let maxVersion = 0;
+	for (let i = 0; i < releases.length; i++) {
+		const match = releaseRe.exec(releases[i]);
+		if (match) {
+			const version = parseInt(match[1]);
+			if (version > maxVersion) {
+				maxVersion = version;
+			}
+		}
+	}
+
+	console.log(`  Maximum existing build for release "${release}" is "${maxVersion}"."`);
+	return maxVersion;
+
+}
+
+async function createRelease(newTag) {
+	console.log(chalk.green(`  Creating release "${newTag}..."`));
+	await gh.repos.createRelease({
+		'owner': owner,
+		'repo': repo,
+		'tag_name': newTag,
+		'name': newTag,
+		'target_commitish': '5d9512d1db135d29ae3ce5a73e8942c07da28a0c'
+	});
+}
+
+async function main() {
+
+	console.log('Attempting to automatically tag BSI release...');
+
+	const isTaggedCommit = (process.env.TRAVIS_TAG !== '');
+	if (isTaggedCommit) {
+		console.log('  Tagged commit, aborting auto-tag.');
+		return;
+	}
+
+	const isPullRequest = (process.env.TRAVIS_PULL_REQUEST !== 'false');
+	if (isPullRequest) {
+		console.log('  Pull request, aborting auto-tag.');
+		return;
+	}
+
+	/*const isFork = (process.env.TRAVIS_SECURE_ENV_VARS === 'false');
+	if (isFork) {
+		console.log('  Cannot publish from forks, aborting auto-tag.');
+		return;
+	}*/
+
+	const branchName = process.env.TRAVIS_BRANCH;
+	const isMaster = (branchName === 'master');
+
+	let release;
+	if (isMaster) {
+		console.log('  Master branch detected.');
+		const release = tryGetActiveDevelopmentRelease();
+		if (release === null) {
+			console.log('  Aborting auto-tag.');
+			return;
+		}
+	} else {
+		console.log(`  Branch detected: "${branchName}".`);
+		if (!versionChecker.test(branchName)) {
+			console.log(`  Branch name "${branchName}" not a valid version, aborting auto-tag.`);
+			return;
+		}
+		release = branchName;
+	}
+
+	let maxVersion = await findMaxVersion(release);
+	const newTag = `v${release}-${++maxVersion}`;
+
+	createRelease(newTag);
+
+}
+
+main();
