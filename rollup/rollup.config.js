c07b13b636d6514b96c955f5b6ffecb8547b17fa
Dave Lockhart <dave.lockhart@d2l.com>
2020-02-25 12:07:43 -0500

    moving uglifyjs build step into rollup (#1902)

--- rollup/rollup.config.js
+++ rollup/rollup.config.js
@@ -1,16 +1,24 @@
 import commonjs from '@rollup/plugin-commonjs';
+import { uglify } from 'rollup-plugin-uglify';
 
 const legacyConfig = {
 	input: './js/bsi.js',
 	plugins: [
 		commonjs()
 	],
-	output: {
-		dir: 'build',
-		format: 'iife',
-		name: 'BSI',
-		sourcemap: true
-	}
+	output: [
+		{
+			file: './build/bsi.js',
+			format: 'iife',
+			name: 'BSI'
+		},
+		{
+			file: './build/bsi.min.js',
+			format: 'iife',
+			name: 'BSI',
+			plugins: [uglify()]
+		}
+	]
 };
 
 export default legacyConfig;
