3a0542b6b3980ce36dbc6d38b03d3ce284b05709
Dave Lockhart <dave.lockhart@d2l.com>
2020-02-26 14:42:35 -0500

    Rollup for CSS/Sass (#1905)
    * moving Sass to rolllup
    
    * no longer producing non-minified bsi.js
    
    * updated deps

--- rollup/rollup.config.js
+++ rollup/rollup.config.js
@@ -1,24 +1,46 @@
 import commonjs from '@rollup/plugin-commonjs';
+import postcss from 'rollup-plugin-postcss';
 import { uglify } from 'rollup-plugin-uglify';
+import autoprefixer from 'autoprefixer';
 
-const legacyConfig = {
+const jsConfig = {
 	input: './js/bsi.js',
 	plugins: [
 		commonjs()
 	],
-	output: [
-		{
-			file: './build/bsi.js',
-			format: 'iife',
-			name: 'BSI'
-		},
-		{
-			file: './build/bsi.min.js',
-			format: 'iife',
-			name: 'BSI',
-			plugins: [uglify()]
-		}
-	]
+	output: {
+		file: './build/bsi.js',
+		format: 'iife',
+		name: 'BSI',
+		plugins: [uglify()]
+	}
 };
 
-export default legacyConfig;
+const cssConfigs = [
+	'./sass/bsi.scss',
+	'./sass/datagrid/datagrid.scss',
+	'./sass/homepages/homepages.scss'
+].map((path) => {
+	const name = path.substring(
+		path.lastIndexOf('/') + 1,
+		path.lastIndexOf('.')
+	);
+	return {
+		input: path,
+		plugins: [
+			postcss({
+				extract: true,
+				minimize: true, /* uses cssnano */
+				plugins: [autoprefixer()],
+				use: [
+					['sass', {outputStyle: 'expanded'}]
+				]
+			})
+		],
+		output: {
+			file: `./build/${name}.css`
+		}
+	};
+});
+
+export default [jsConfig].concat(cssConfigs);
