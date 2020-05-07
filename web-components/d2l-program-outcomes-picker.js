e2a130ecf91668790183688dd385761b43c41381
Ryan Goessl <Ryan.Goessl@D2L.com>
2020-05-07 16:01:39 -0400

    Bump program-outcomes-picker

--- web-components/d2l-program-outcomes-picker.js
+++ web-components/d2l-program-outcomes-picker.js
@@ -1,2 +1,3 @@
 import 'd2l-program-outcomes-picker/d2l-program-outcomes-picker.js';
 import 'd2l-program-outcomes-picker/d2l-asn-outcomes-picker.js';
+import 'd2l-program-outcomes-picker/d2l-unlink-outcomes-picker.js';
