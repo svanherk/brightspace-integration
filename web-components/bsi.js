08bc98599add43b3e036f7b7dd95b44bf1ff2815
Dave Lockhart <dave.lockhart@d2l.com>
2020-05-26 10:27:08 -0400

    referencing tooltip from core (#2131) [skip release]

--- web-components/bsi.js
+++ web-components/bsi.js
@@ -24,6 +24,7 @@ import '@brightspace-ui/core/components/menu/menu-item-separator.js';
 import '@brightspace-ui/core/components/menu/menu-item.js';
 import '@brightspace-ui/core/components/menu/menu.js';
 import '@brightspace-ui/core/components/more-less/more-less.js';
+import '@brightspace-ui/core/components/tooltip/tooltip.js';
 
 import 'd2l-activities/components/d2l-subtitle/d2l-subtitle.js';
 import '@d2l/switch/d2l-switch.js';
