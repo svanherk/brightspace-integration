ba7b5882ffd2e4bc0541cf248c314027fdf94b62
Dave Lockhart <dave.lockhart@d2l.com>
2020-07-24 13:56:28 -0400

    unbundling subtle button (#2322)

--- rollup/rollup-wc.config.js
+++ rollup/rollup-wc.config.js
@@ -11,6 +11,7 @@ const componentFiles = [
 	'./node_modules/@brightspace-ui/core/components/breadcrumbs/breadcrumb-current-page.js',
 	'./node_modules/@brightspace-ui/core/components/breadcrumbs/breadcrumbs.js',
 	'./node_modules/@brightspace-ui/core/components/button/button-icon.js',
+	'./node_modules/@brightspace-ui/core/components/button/button-subtle.js',
 	'./node_modules/@brightspace-ui/core/components/button/floating-buttons.js',
 	'./node_modules/@brightspace-ui/core/components/dropdown/dropdown-context-menu.js',
 	'./node_modules/@brightspace-ui/core/components/inputs/input-checkbox.js',
