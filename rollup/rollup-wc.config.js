eb8e4e37918ca7fc9d56c96528c086bfc2e8894b
Bryan Mailloux <Bryan.Mailloux@D2L.com>
2020-08-19 12:51:31 -0400

    Add brightspace-ui-labs pagination component

--- rollup/rollup-wc.config.js
+++ rollup/rollup-wc.config.js
@@ -35,6 +35,7 @@ const componentFiles = [
 	'./node_modules/@brightspace-ui/core/components/switch/switch.js',
 	'./node_modules/@brightspace-ui/core/components/switch/switch-visibility.js',
 	'./node_modules/@brightspace-ui/core/components/tooltip/tooltip.js',
+	'./node_modules/@brightspace-ui-labs/pagination/pagination.js',
 	'./node_modules/d2l-activities/components/d2l-subtitle/d2l-subtitle.js',
 	'./node_modules/d2l-button-group/d2l-button-group.js',
 	'./node_modules/d2l-button-group/d2l-action-button-group.js',
